e4327132dd373af1ef3d4f338d3654f319be6dcc
Luka Tomic <tluka234@gmail.com>
2024-08-24 18:02:22 +0200

    Sitemap and robots.txt

--- backend/server.js
+++ backend/server.js
@@ -61,8 +61,8 @@ const sessionConfig = {
   saveUninitialized: false,
   cookie: {
     httpOnly: true,
-    secure: true,
-    sameSite: "None",
+    // secure: true,
+    // sameSite: "Strict",
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
     maxAge: 1000 * 60 * 60 * 24 * 7,
   },
